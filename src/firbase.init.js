15ee32794d018757f9b4d149693d1725f9cbfc87
jahir <nockjahirul@gmail.com>
2024-11-28 09:37:20 +0600

    add route set and other set and navbar componnent section

---
+++ src/firbase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_2Gd4d1UTtdY_6QFwrXbWRTLTPoCBAnI",
+  authDomain: "auth-moha-milon-d051a.firebaseapp.com",
+  projectId: "auth-moha-milon-d051a",
+  storageBucket: "auth-moha-milon-d051a.firebasestorage.app",
+  messagingSenderId: "631059930948",
+  appId: "1:631059930948:web:a27f61e2e492cc6100c111"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
